86fea592b8ecf31e9f46471c19cef6e7e3463823
Dennis Bingham <dennis.bingham.dev@gmail.com>
2021-10-03 11:12:30 -0600

    updated file

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// Swift "equivalent" to an interface
+protocol Receiver
+{
+    func receiveThis()
+}
+
+// Constructor Dependency Injection
+// Class taking a Receiver type and it must be set through
+// the initializer init()
+class Sender
+{
+    private var receiver: Receiver
+    
+    init(r: Receiver)
+    {
+        receiver = r
+    }
+    
+    func doSomething()
+    {
+        receiver.receiveThis()
+    }
+}
+
+class SpecificReceiver: Receiver
+{
+    func receiveThis() {
+        // do something interesting.
+    }
+}
